513f0f63fb18eccb33341d166526ee6d3e8d1b09
ewgenius <ewgeniux@gmail.com>
2016-08-03 09:03:15 +0300

    init scene

--- src/nanocollider.ts
+++ src/nanocollider.ts
@@ -1,3 +1,16 @@
-import {Scene} from 'three'
+import * as THREE from 'three'
+import {Scene, WebGLRenderer, PerspectiveCamera} from 'three'
 
-console.log(Scene)
+function main() {
+  const renderer = new WebGLRenderer({
+    canvas: document.getElementById('canvas') as HTMLCanvasElement
+  })
+  const scene = new Scene
+  const camera = new PerspectiveCamera(65, 800 / 600, 1, 10000)
+
+  requestAnimationFrame(() => {
+    renderer.render(scene, camera)
+  })
+}
+
+main()
