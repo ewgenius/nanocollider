f9e5135789a10d36980c5f313ec11d39ae322e68
ewgenius <ewgeniux@gmail.com>
2016-08-03 09:51:42 +0300

    upd

--- src/nanocollider.ts
+++ src/nanocollider.ts
@@ -1,5 +1,14 @@
 import * as THREE from 'three'
-import {Scene, WebGLRenderer, PerspectiveCamera} from 'three'
+import {
+  Scene,
+  WebGLRenderer,
+  PerspectiveCamera,
+  Mesh,
+  CubeGeometry,
+  MeshNormalMaterial
+} from 'three'
+
+console.log(THREE)
 
 function main() {
   const renderer = new WebGLRenderer({
@@ -7,6 +16,13 @@ function main() {
   })
   const scene = new Scene
   const camera = new PerspectiveCamera(65, 800 / 600, 1, 10000)
+  camera.position.y = 150
+	camera.position.z = 350
+
+  const cube = new Mesh(new CubeGeometry(100, 100, 100), new MeshNormalMaterial())
+  //const cube = new Mesh
+
+  scene.add(cube)
 
   requestAnimationFrame(() => {
     renderer.render(scene, camera)
