2babfc342e8b38f2fe8302de224d32ebace01cbe
ewgenius <ewgeniux@gmail.com>
2016-08-03 08:50:41 +0300

    init

---
+++ webpack.config.dev.js
@@ -0,0 +1,23 @@
+var webpack = require('webpack')
+
+module.exports = {
+  entry: {
+    app: ['babel-polyfill', './.temp/nanocollider.js'],
+    vendor: require('./config/vendor')
+  },
+  output: {
+    filename: '[name].bundle.js',
+    path: './dist'
+  },
+  module: {
+    loaders: require('./config/loaders')
+  },
+  plugins: [
+    new webpack.optimize.CommonsChunkPlugin('vendor', 'vendor.bundle.js', Infinity),
+    new webpack.DefinePlugin({
+      "process.env": {
+        NODE_ENV: JSON.stringify("development")
+      }
+    })
+  ]
+}
